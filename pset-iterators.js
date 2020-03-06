0edbb745b4a1e11da10a55623eb93266440c6c42
oona baby <oonababy@noahs-mbp-2.lan>
2020-03-06 13:34:36 -0500

    completed homework

--- pset-iterators.js
+++ pset-iterators.js
@@ -43,10 +43,15 @@ console.log('Problem 1:')
 
 // Add your code below this line
 
+const firstWhigPresident =
+presidents.find((president) => {
+  return president.party === "Whig"
+})
+
 // Add your code above this line
 
 /** added for formatting purposes **/
-console.log('')
+console.log(firstWhigPresident)
 console.log('-----------------')
 
 /***********
@@ -66,10 +71,14 @@ console.log('Problem 2:')
 
 // Add your code below this line
 
+const presidentsNamedJames = presidents.filter((presidents) => {
+  return presidents.president.includes('James')
+})
+
 // Add your code above this line
 
 /** added for formatting purposes **/
-console.log('')
+console.log(presidentsNamedJames)
 console.log('-----------------')
 
 /***********
@@ -93,10 +102,14 @@ console.log('Problem 3:')
 
 // Add your code below this line
 
+const presidentialParties = presidents.map((presidents) => {
+  return presidents.party
+})
+
 // Add your code above this line
 
 /** added for formatting purposes **/
-console.log('')
+console.log(presidentialParties)
 console.log('-----------------')
 
 /***********
@@ -115,10 +128,19 @@ console.log('Problem 4:')
 
 // Add your code below this line
 
+let startDate = "1850-01-01"
+let endDate = "1900-12-31"
+
+const presidentsBetween1850and1900 = presidents.filter((presidents) => {
+  return presidents.took_office > startDate && presidents.took_office < endDate
+})
+
 // Add your code above this line
 
+
+
 /** added for formatting purposes **/
-console.log('')
+console.log(presidentsBetween1850and1900)
 console.log('-----------------')
 
 /***********
@@ -138,10 +160,14 @@ console.log('Problem 5:')
 
 // Add your code below this line
 
+const livingPresidents = presidents.filter((presidents) => {
+  return presidents.death_year === null
+})
+
 // Add your code above this line
 
 /** added for formatting purposes **/
-console.log('')
+console.log(livingPresidents)
 console.log('-----------------')
 
 /***********
@@ -161,10 +187,14 @@ console.log('Problem 6:')
 
 // Add your code below this line
 
+const firstRepublican = presidents.find((president) => {
+  return president.party === "Republican"
+})
+
 // Add your code above this line
 
 /** added for formatting purposes **/
-console.log('')
+console.log(firstRepublican)
 console.log('-----------------')
 
 /***********
@@ -184,10 +214,17 @@ console.log('Problem 7:')
 
 // Add your code below this line
 
+const shortTermPresidents = presidents.filter((presidents) => {
+  let termLength = parseInt(presidents.left_office) - parseInt(presidents.took_office) ;
+  if(termLength < 4) {
+    return presidents
+  }
+})
+
 // Add your code above this line
 
 /** added for formatting purposes **/
-console.log('')
+console.log(shortTermPresidents)
 console.log('-----------------')
 
 /***********
@@ -211,10 +248,28 @@ console.log('Problem 8:')
 
 // Add your code below this line
 
+const firstNames = presidents.map((firstName) => {
+  return firstName.president
+})
+
+console.log(firstNames)
+
+const firstNameCount = presidents.reduce((sum, president) => {
+  const fullName = president.president;
+  const firstName = fullName.split(' ')[0];
+
+  if (sum[firstName] === undefined) {
+    sum[firstName] = 1
+  } else {
+    sum[firstName] += 1
+  }
+  return sum
+}, {})
+
 
 
 // Add your code above this line
 
 /** added for formatting purposes **/
-console.log('')
+console.log(firstNameCount)
 console.log('-----------------')
